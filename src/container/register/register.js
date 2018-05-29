ab9f1f848cd54116f6301e15675735dcb6fc8bef
queena <772905605@qq.com>
2018-05-29 11:47:27 +0800

    实现登录注册及数据存储

--- src/container/register/register.js
+++ src/container/register/register.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Logo from '../../component/logo/logo'
 import { List, InputItem, Radio, WingBlank, WhiteSpace, Button } from 'antd-mobile'
 import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 import { register } from '../../redux/user.redux'
 
 @connect(
@@ -32,6 +33,7 @@ class Register extends React.Component{
 		const RadioItem = Radio.RadioItem
 		return (
 			<div>
+				{this.props.redirectTo? <Redirect to={this.props.redirectTo} />:null}
 				<Logo></Logo>
 				<WingBlank>
 					<List>
--- src/redux/user.redux.js
+++ src/redux/user.redux.js
@@ -1,33 +1,62 @@
 import axios from 'axios'
+import {getRedirectPath} from '../util'
 
 const REGISTER_SUCCESS = 'REGISTER_SUCCESS'
+const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
 const ERROR_MSG = 'ERROR_MSG'
+const LOAD_DATA = 'LOAD_DATA'
 
 const initState={
+	redirectTo:'',
 	isAuth:false,
 	msg:'',
 	user:'',
-	pwd:'',
 	type:''
 }
 export function user(state=initState, action){
 	switch(action.type){
 		case REGISTER_SUCCESS:
-		return {...state, msg:'', isAuth:true,...action.payload}
+			return {...state, msg:'', redirectTo:getRedirectPath(action.payload), isAuth:true,...action.payload}
+		case LOGIN_SUCCESS:
+			return {...state, msg:'', redirectTo:getRedirectPath(action.payload), isAuth:true,...action.payload}
+		case LOAD_DATA:
+			return {...state, ...action.payload}
 		case ERROR_MSG:
-		return {...state,isAuth:false,msg:action.msg}
+			return {...state,isAuth:false,msg:action.msg}
 		default:
 			return state
 	}
 	return state
 }
 
-function registerSuccess(data){
+function registerSuccess(obj){
+	const {pwd, ...data} = obj;
 	return {type:REGISTER_SUCCESS, payload:data}
 }
-
+function loginSuccess(data){
+	return {type:LOGIN_SUCCESS, payload:data}
+}
 function errorMsg(msg){
 	return {msg, type:ERROR_MSG}
+}
+
+export function loadData(userinfo){
+	return {type:LOAD_DATA,payload:userinfo}
+}
+export function login({user,pwd}){
+	if(!user||!pwd){
+		return errorMsg('用户名密码必须输入')
+	}
+	return dispatch=>{
+		axios.post('/user/login',{user,pwd})
+			.then(res=>{
+				if(res.status==200&&res.data.code===0){
+					dispatch(loginSuccess(res.data.data))
+				}else{
+					dispatch(errorMsg(res.data.msg))
+				}
+			})
+	}
 }
 
 export function register({user,pwd,repeatpwd,type}){
